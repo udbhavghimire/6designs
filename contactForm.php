1bc35624ddd1721ee66368ba9f202835a3f5ee60
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-06-11 10:07:53 +0545

    email

--- contactForm.php
+++ contactForm.php
@@ -15,7 +15,7 @@ $mail->isSMTP(); // Set mailer to use SMTP
 $mail->Host = 'mail.sixdesign.ca'; // Specify main and backup SMTP servers
 $mail->SMTPAuth = true; // Enable SMTP authentication
 $mail->Username = 'info@sixdesign.ca'; // SMTP username
-$mail->Password = 'mail@sixdesign'; // SMTP password
+$mail->Password = 'sixdesign123'; // SMTP password
 $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587; // TCP port to connect to
 
@@ -29,46 +29,46 @@ $mail->Subject = "Sixdesign - Landing Page Inquiry";
 $message = '<html>
 
 <body>';
-    $message .= '<table rules="all" style="border:none;" cellpadding="3">';
-        $message .= "<tr>
+$message .= '<table rules="all" style="border:none;" cellpadding="3">';
+$message .= "<tr>
             <td><strong>Name:</strong> </td>
             <td>" . strip_tags($_POST['name']) . "</td>
         </tr>";
-        $message .= "<tr>
+$message .= "<tr>
             <td><strong>Phone:</strong> </td>
             <td>" . strip_tags($_POST['phone']) . "</td>
         </tr>";
-        $message .= "<tr>
+$message .= "<tr>
             <td><strong>Email:</strong> </td>
             <td>" . strip_tags($_POST['email']) . "</td>
         </tr>";
-        $message .= "<tr>
+$message .= "<tr>
             <td><strong>Message : </strong> </td>
             <td>" . strip_tags($_POST['message']) . "</td>
         </tr>";
-        $message .= "<tr>
+$message .= "<tr>
             <td><strong>Source : </strong> </td>
             <td>sixdesign.ca</td>
         </tr>";
-        $message .= "</table>";
-    $message .= "</body>
+$message .= "</table>";
+$message .= "</body>
 
 </html>";
 
 $mail->Body = $message;
-$mail->AltBody = $_POST['message'].$_POST['email'].$_POST['name'].$_POST['phone'];
+$mail->AltBody = $_POST['message'] . $_POST['email'] . $_POST['name'] . $_POST['phone'];
 
-if(!$mail->send()) {
+if (!$mail->send()) {
     $_SESSION["error"] = "Application not submitted!";
-    
-        header('Location: index.php');
-        exit();   
-    
+
+    header('Location: index.php');
+    exit();
+
 } else {
     $_SESSION["success"] = "Application submitted.";
-        header('Location: ./');
-        exit();   
-    
+    header('Location: ./');
+    exit();
+
 }
 
 ?>
