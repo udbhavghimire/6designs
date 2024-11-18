edd2459c943506bf4ac572d351d473486366f761
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-11-18 10:15:49 +0545

    changes

--- components/floating-button.php
+++ components/floating-button.php
@@ -1,4 +1,4 @@
-<a href="#Contact" class="sticky-bottom fixcontact d-md-none text-decoration-none" target="_blank">
+<a href="#Contact" class="sticky-bottom fixcontact d-md-none text-decoration-none">
     <div class="shadow-lg rounded-mine d-flex flex-column">
         <div class="btn my-sm-0 d-flex align-items-center justify-content-center gap-3 whatsapp-btn py-2">
             <div class="d-flex align-items-center gap-2">
--- index.php
+++ index.php
@@ -483,7 +483,7 @@
         </div>
     </div>
 
-    <div><a href="#Contact" class="sticky-bottom fixcontact d-md-none text-decoration-none" target="_blank">
+    <div><a href="#Contact" class="sticky-bottom fixcontact d-md-none text-decoration-none">
             <div class="shadow-lg rounded-mine d-flex flex-column">
                 <div class="btn my-sm-0 d-flex align-items-center justify-content-center gap-3 whatsapp-btn py-2">
                     <div class="d-flex align-items-center gap-2">
