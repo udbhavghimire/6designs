2c730f08c5ed1bbeae52cfa108e4b1088dabbf26
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-11-18 20:32:11 +0545

    changes

--- components/floating-button.php
+++ components/floating-button.php
@@ -1,21 +1,16 @@
-<div class="sticky-bottom fixcontact d-md-none d-flex justify-content-center gap-2">
-    <a href="https://wa.me/16475274970" class="text-decoration-none" target="_blank">
-        <div class="shadow-lg rounded-mine">
-            <div class="btn my-sm-0 d-flex align-items-center justify-content-center gap-2 whatsapp-btn py-2 px-3">
-                <i class="bi bi-whatsapp text-white whatsapp-icon"></i>
-                <span class="text-white">
-                    <b>647-527-4970</b>
-                </span>
-            </div>
-        </div>
-    </a>
+<div class="sticky-bottom fixcontact d-md-none d-flex justify-content-center gap-4">
     <a href="#Contact" class="text-decoration-none">
         <div class="shadow-lg rounded-mine">
-            <div class="btn my-sm-0 d-flex align-items-center justify-content-center gap-2 contact-btn py-2 px-3">
+            <div class="btn my-sm-0 d-flex align-items-center justify-content-center gap-5 contact-btn py-2 px-4">
                 <span class="text-white">
                     <b>Contact us</b>
                 </span>
             </div>
+        </div>
+    </a>
+    <a href="https://wa.me/16475274970" class="text-decoration-none" target="_blank">
+        <div class="d-flex align-items-center justify-content-center whatsapp-img ">
+            <img src="../images/whatsapp.png" alt="whatsapp" class=" whatsapp-img">
         </div>
     </a>
 </div>
