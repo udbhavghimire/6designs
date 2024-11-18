7ef9844f4d0f16e2466bf6ce1082e69bd03f2945
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-11-18 10:13:06 +0545

    changes

--- components/floating-button.php
+++ components/floating-button.php
@@ -1,10 +1,10 @@
-<a href="https://wa.me/6475274970" class="sticky-bottom fixcontact d-md-none text-decoration-none" target="_blank">
+<a href="#Contact" class="sticky-bottom fixcontact d-md-none text-decoration-none" target="_blank">
     <div class="shadow-lg rounded-mine d-flex flex-column">
         <div class="btn my-sm-0 d-flex align-items-center justify-content-center gap-3 whatsapp-btn py-2">
             <div class="d-flex align-items-center gap-2">
                 <i class="bi bi-whatsapp text-white whatsapp-icon"></i>
                 <span class="text-white">
-                    <b>647-527-4970</b>
+                    <b>Contact us</b>
                 </span>
             </div>
         </div>
--- index.php
+++ index.php
@@ -483,14 +483,13 @@
         </div>
     </div>
 
-    <div><a href="https://wa.me/6475274970" class="sticky-bottom fixcontact d-md-none text-decoration-none"
-            target="_blank">
+    <div><a href="#Contact" class="sticky-bottom fixcontact d-md-none text-decoration-none" target="_blank">
             <div class="shadow-lg rounded-mine d-flex flex-column">
                 <div class="btn my-sm-0 d-flex align-items-center justify-content-center gap-3 whatsapp-btn py-2">
                     <div class="d-flex align-items-center gap-2">
                         <i class="bi bi-whatsapp text-white whatsapp-icon"></i>
                         <span class="text-white">
-                            <b>647-527-4970</b>
+                            <b>Contact us</b>
                         </span>
                     </div>
                 </div>
