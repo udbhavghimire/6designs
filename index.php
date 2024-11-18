da284a9bab9ec3f21422fa3c052e6d21f206d51c
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-11-18 20:10:53 +0545

    changes

--- index.php
+++ index.php
@@ -483,18 +483,27 @@
         </div>
     </div>
 
-    <div><a href="#Contact" class="sticky-bottom fixcontact d-md-none text-decoration-none">
-            <div class="shadow-lg rounded-mine d-flex flex-column">
-                <div class="btn my-sm-0 d-flex align-items-center justify-content-center gap-3 whatsapp-btn py-2">
-                    <div class="d-flex align-items-center gap-2">
-                        <i class="bi bi-whatsapp text-white whatsapp-icon"></i>
-                        <span class="text-white">
-                            <b>Contact us</b>
-                        </span>
-                    </div>
+    <div class="sticky-bottom fixcontact d-md-none d-flex justify-content-center gap-2">
+        <a href="https://wa.me/16475274970" class="text-decoration-none" target="_blank">
+            <div class="shadow-lg rounded-mine">
+                <div class="btn my-sm-0 d-flex align-items-center justify-content-center gap-2 whatsapp-btn py-2 px-3">
+                    <i class="bi bi-whatsapp text-white whatsapp-icon"></i>
+                    <span class="text-white">
+                        <b>647-527-4970</b>
+                    </span>
                 </div>
             </div>
-        </a></div>
+        </a>
+        <a href="#Contact" class="text-decoration-none">
+            <div class="shadow-lg rounded-mine">
+                <div class="btn my-sm-0 d-flex align-items-center justify-content-center gap-2 contact-btn py-2 px-3">
+                    <span class="text-white">
+                        <b>Contact us</b>
+                    </span>
+                </div>
+            </div>
+        </a>
+    </div>
 
 
     <img src="images/mouse.png" class="foot-up-img" alt="" data-aos="fade-up" data-aos-duration="2000">
