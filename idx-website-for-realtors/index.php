2a9db979a3e43f010f5c7d117728d6aed1328b53
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-11-22 16:27:04 +0545

    changessss

--- idx-website-for-realtors/index.php
+++ idx-website-for-realtors/index.php
@@ -6,11 +6,7 @@ require_once(__DIR__ . '/../config/cities.php');
 // Get city from URL parameter
 $city_slug = $_GET['city'] ?? '';
 
-// Check if city exists in config
-if (!isset($cities[$city_slug])) {
-    header('Location: ' . $base_url . '404.php');
-    exit;
-}
+
 
 $city = $cities[$city_slug];
 include_once(__DIR__ . '/city-template.php');
